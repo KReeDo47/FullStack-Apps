42949b8e262b9099c12ce451594da8ffb596e88c
KReeDo47 <dzelyalov.a.i.21@gmail.com>
2023-01-11 13:55:13 +0300

    fullstack-app

--- backend/config/database.js
+++ backend/config/database.js
@@ -4,7 +4,7 @@ import mysql from "mysql2";
 const db = mysql.createConnection({
   host: "localhost",
   user: "root",
-  password: "1234",
+  password: "Ali542434063004",
   database: "pos_db",
 });
 
